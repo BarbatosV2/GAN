b3326d6dc35bbfd261e9f46713de8bd7e8ab8246
Rampardos <63419320+BarbatosV2@users.noreply.github.com>
2024-05-29 21:46:42 +0930

    updated conv2d for better training

--- models.py
+++ models.py
@@ -5,39 +5,52 @@ import torch.nn as nn
 class Generator(nn.Module):
     def __init__(self):
         super(Generator, self).__init__()
-        self.model = nn.Sequential(
-            nn.Linear(100, 256),
-            nn.LeakyReLU(0.2),
-            nn.BatchNorm1d(256),
-            nn.Linear(256, 512),
-            nn.LeakyReLU(0.2),
-            nn.BatchNorm1d(512),
-            nn.Linear(512, 1024),
-            nn.LeakyReLU(0.2),
-            nn.BatchNorm1d(1024),
-            nn.Linear(1024, 64 * 64 * 3),
-            nn.Tanh()
+        self.init_size = 64 // 4
+        self.l1 = nn.Sequential(nn.Linear(100, 128 * self.init_size ** 2))
+
+        self.conv_blocks = nn.Sequential(
+            nn.BatchNorm2d(128),
+            nn.Upsample(scale_factor=2),
+            nn.Conv2d(128, 128, 3, stride=1, padding=1),
+            nn.BatchNorm2d(128, 0.8),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Upsample(scale_factor=2),
+            nn.Conv2d(128, 64, 3, stride=1, padding=1),
+            nn.BatchNorm2d(64, 0.8),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(64, 3, 3, stride=1, padding=1),
+            nn.Tanh(),
         )
 
-    def forward(self, x):
-        x = self.model(x)
-        x = x.view(x.size(0), 3, 64, 64)
-        return x
+    def forward(self, z):
+        out = self.l1(z)
+        out = out.view(out.shape[0], 128, self.init_size, self.init_size)
+        img = self.conv_blocks(out)
+        return img
 
 # Define the Discriminator
 class Discriminator(nn.Module):
     def __init__(self):
         super(Discriminator, self).__init__()
+
+        def discriminator_block(in_filters, out_filters, bn=True):
+            block = [nn.Conv2d(in_filters, out_filters, 3, 2, 1), nn.LeakyReLU(0.2, inplace=True), nn.Dropout2d(0.25)]
+            if bn:
+                block.append(nn.BatchNorm2d(out_filters, 0.8))
+            return block
+
         self.model = nn.Sequential(
-            nn.Flatten(),
-            nn.Linear(64 * 64 * 3, 512),
-            nn.LeakyReLU(0.2),
-            nn.Linear(512, 256),
-            nn.LeakyReLU(0.2),
-            nn.Linear(256, 1),
-            nn.Sigmoid()
+            *discriminator_block(3, 16, bn=False),
+            *discriminator_block(16, 32),
+            *discriminator_block(32, 64),
+            *discriminator_block(64, 128),
         )
 
-    def forward(self, x):
-        x = self.model(x)
-        return x
+        ds_size = 64 // 2 ** 4
+        self.adv_layer = nn.Sequential(nn.Linear(128 * ds_size ** 2, 1), nn.Sigmoid())
+
+    def forward(self, img):
+        out = self.model(img)
+        out = out.view(out.shape[0], -1)
+        validity = self.adv_layer(out)
+        return validity
